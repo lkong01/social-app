de5c3bc414e9802e6de4491546809a82db8b9e65
k <lingweikong10@gmail.com>
2022-08-28 13:00:38 -0500

    Add user controller

---
+++ controllers/userControllers.js
@@ -0,0 +1,36 @@
+const User = require("../models/user");
+
+// Display detail page for a specific User.
+exports.user_detail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: User detail: ${req.params.id}`);
+};
+
+// Display User create form on GET.
+exports.user_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: User create GET");
+};
+
+// Handle User create on POST.
+exports.user_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: User create POST");
+};
+
+// Display User delete form on GET.
+exports.user_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: User delete GET");
+};
+
+// Handle User delete on POST.
+exports.user_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: User delete POST");
+};
+
+// Display User update form on GET.
+exports.user_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: User update GET");
+};
+
+// Handle User update on POST.
+exports.user_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: User update POST");
+};
